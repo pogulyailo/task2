8844a7eb1fd71ffb98bba164b5deeac16c5bc3cc
igr <mixa_fc@ukr.net>
2017-09-05 18:40:39 +0300

    вторая задача прекурса

---
+++ src/task2.java
@@ -0,0 +1,13 @@
+public class task2 {
+    public static void main(String[] args) {
+
+        int s1 = 17;
+        int s2 = (int) (s1 / 10);
+        int s3 = (int) (s1 % 10);
+        int s = (int)(s2 + s3);
+        int n = (int) (s);
+
+        System.out.println(n);
+    }
+
+}
